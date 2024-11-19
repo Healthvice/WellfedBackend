6418353a6659b6325e38bacca3e2ce08d7d057d1
rithvikvelapati <rithvik.velapati@thehealthvice.com>
2024-11-19 13:50:35 -0600

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // Use process.env.PORT or fallback to 3000
-  const port = process.env.PORT || 3001;
+  // Use process.env.PORT for Azure App Service, fallback to 3000 for local development
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
